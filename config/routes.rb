8cb8fe69eb4472091d4e4430aaaa14dc2320fea6
Solomon Sunday <ssdahymond@gmail.com>
2019-04-24 23:22:35 +0100

    session manipulation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ get 'about', to: 'pages#about'
 resources :articles
 
 get 'signup', to: 'users#new'
+resources :users, except: [:new]
+
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+
 #post 'users', to: 'users#create'
 
 resources :users, except: [:new]
