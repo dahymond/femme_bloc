1ff40a764cf99d86deab386946f904f150699108
Solomon Sunday <ssdahymond@gmail.com>
2019-04-26 22:03:46 +0100

    authentications and session control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
@@ -18,17 +19,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
 
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "User updated successfully"
       redirect_to user_path(@user)
@@ -40,5 +41,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
